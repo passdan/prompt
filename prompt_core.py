271c1c134507713aa5cdaa9ced4a452bf5b18160
Daniel Pass <daniel.antony.pass@googlemail.com>
2013-08-23 16:16:20 +0100

    initial structure

--- prompt_core.py
+++ prompt_core.py
@@ -2,27 +2,40 @@
 #prompt-core-processes
 #daniel.antony.pass@googlemail.com
 
+import getopt
+args = '-i'.split()
+
 from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastnCommandline
 
-for seq_record in SeqIO.parse("test.fasta", "fasta"):
-    print seq_record.id
-    
+
+in_fasta = "18s_small.fas"
+hand = in_fasta = s/(.*).fasta/\1/
 
 def main():
     #process infiles
 
     #cd-hit
 
-    #blastn
+    #do blast
+    blastn(in_fasta)
 
     #create outfiles
 
     #build web files
 
 
-def blastn(in_fasta):
-	#do blastn
-	blastn_cline = NcbiblastnCommandline(query=in_fasta, db="nr", evalue=0.001, outfmt=8, out="opuntia.blastn")    
+def blastn(fas):
+    #do blastn
+    blastn_cline = NcbiblastnCommandline(query=fas, db="~/projects/MetaMOD/blastdb/dia_db", evalue=0.001, outfmt=6, out=".blastn")
 
+    
+    blastn_cline()
 
+    #stdout, stderr = blastn_cline()
+
+#for seq_record in SeqIO.parse("18s.fas", "fasta"):
+#    blastn(seq_record.fasta)
+
+if __name__ == "__main__":
+    main()
