3cee1a0e5c477f9736defde0f4ae8e7a0766c1bf
Daniel <daniel.antony.pass@googlemail.com>
2013-10-03 00:56:59 +0100

    core work

--- prompt_core.py
+++ prompt_core.py
@@ -2,23 +2,39 @@
 #prompt-core-processes
 #daniel.antony.pass@googlemail.com
 
+import sys
 import getopt
 args = '-i'.split()
 
 from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastnCommandline
 
+in_fasta = 0
+seq_no = 0
+lengths = []
 
-in_fasta = "18s_small.fas"
-hand = in_fasta = s/(.*).fasta/\1/
+try:
+    in_fasta = open("test_data/18s.fas", "rU")
+except IOError:
+    print "Cannot find in_fasta"
+
 
 def main():
     #process infiles
+    print "Processing infiles"
 
+    for record in SeqIO.parse(in_fasta, "fasta"):
+        lengths.append(len(record.seq))
+    
+    global seq_no
+    seq_no = len(lengths)
+
+    print "Average length of", seq_no, "input sequences is ", sum(lengths)/seq_no, "base pairs"
     #cd-hit
+    print "Passing to CD-HIT for data reduction"
 
     #do blast
-    blastn(in_fasta)
+    #blastn(in_fasta)
 
     #create outfiles
 
@@ -27,10 +43,11 @@ def main():
 
 def blastn(fas):
     #do blastn
-    blastn_cline = NcbiblastnCommandline(query=fas, db="~/projects/MetaMOD/blastdb/dia_db", evalue=0.001, outfmt=6, out=".blastn")
+    pass
 
-    
-    blastn_cline()
+    #blastn_cline = NcbiblastnCommandline(query=fas, db="~/projects/MetaMOD/blastdb/dia_db", evalue=0.001, outfmt=6, out=".blastn")
+
+    #blastn_cline()
 
     #stdout, stderr = blastn_cline()
 
