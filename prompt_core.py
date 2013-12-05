237b70dba5c659e7d048425741a464531c15fa55
passdan <daniel.antony.pass@googlemail.com>
2013-12-05 23:26:09 +0000

    blast and cd-hit running

--- prompt_core.py
+++ prompt_core.py
@@ -7,17 +7,22 @@ import sys
 import getopt
 args = '-i'.split()
 
+import re
 from subprocess import call
-
 from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastnCommandline
 
 in_fasta = 0
 seq_no = 0
 lengths = []
+in_fasta = "18s.fas"
+database = "blast_db/randomised_diatom.fasta"
+indir = "test_data/"
+tmpdir = "tmp_dir/"
+
 
 try:
-    in_fasta = open("test_data/18s.fas", "rU")
+    open_fasta = open(indir + in_fasta, "rU")
 except IOError:
     print "Cannot find input fasta"
 
@@ -26,7 +31,7 @@ def main():
     #process infiles
     print "Processing infiles"
 
-    for record in SeqIO.parse(in_fasta, "fasta"):
+    for record in SeqIO.parse(open_fasta, "fasta"):
         lengths.append(len(record.seq))
     
     global seq_no
@@ -35,30 +40,47 @@ def main():
     print "Average length of", seq_no, "input sequences is ", sum(lengths)/seq_no, "base pairs"
 
     #cd-hit
-    print "Passing to CD-HIT for data reduction"
-
-    call(["cd-hit-454" + "-i" + "test_data/18s.fas"  + "-o cd-hit-out.fa"])
+    #print "Passing to CD-HIT for data reduction"
+    cdhit(in_fasta)
 
     #do blast
-    #blastn(in_fasta)
+    #blastn(in_fasta, database)
+        ##can this take the cdhit output?
 
     #create outfiles
+    ##Merge blast and cdhit
+
 
     #build web files
 
 
-def blastn(fas):
-    #do blastn
-    pass
+def blastn(fas, db):
+    print("blasting " + str(seq_no) + " sequences against the " + db + " database")
 
-    #blastn_cline = NcbiblastnCommandline(query=fas, db="~/projects/MetaMOD/blastdb/dia_db", evalue=0.001, outfmt=6, out=".blastn")
+    blast_in = (indir + fas)
+    blast_out = (tmpdir + fas + ".blastn")
 
-    #blastn_cline()
 
-    #stdout, stderr = blastn_cline()
+    blastn_cline = NcbiblastnCommandline(query=blast_in, db=db, evalue=0.001, outfmt=6, out=blast_out, num_threads=4)
 
-#for seq_record in SeqIO.parse("18s.fas", "fasta"):
-#    blastn(seq_record.fasta)
+    stdout, stderr = blastn_cline()
+
+    blast_err_log = open(tmpdir + "blast_err.txt", "w")
+    blast_stdout_log = open(tmpdir + "blast_stdout.txt", "w")
+
+    blast_err_log.write(stderr)
+    blast_stdout_log.write(stdout)
+
+
+def cdhit(fas):
+    cdhit_in = (indir + fas)
+    global cdhit_out
+    cdhit_out = (tmpdir + fas + "_cdhitout.fa")
+    cd_stdout = (tmpdir + "cdhit_stdout.txt")
+    cd_stderr = (tmpdir + "cdhit_sterr.txt")
+
+
+    call(["cd-hit-454","-i",cdhit_in,"-o",cdhit_out])
 
 if __name__ == "__main__":
     main()
