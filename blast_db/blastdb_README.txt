8740c1610f8ac991d6513b5b6b7d87ef35061afb
passdan <daniel.antony.pass@googlemail.com>
2014-10-18 23:20:46 +0100

    config tweaks

--- blast_db/blastdb_README.txt
+++ blast_db/blastdb_README.txt
@@ -1,3 +1,5 @@
 This is the directory where your blast files would be located.
 
+A blast database must be gegnerated from a fasta reference file where the sequences are annotated with the same headers as the taxa_dictionary.txt found in the taxonomy directory.
+
 Formatting information to be expanded on.
--- launch_prompt.py
+++ launch_prompt.py
@@ -1,39 +1,60 @@
 #!/usr/bin/python
-
-
+  
 import sys
 import ConfigParser
 from subprocess import call
-
+ 
+conf_file = sys.argv[1]
+    
 Config = ConfigParser.ConfigParser()
-
-
-sample_dir = "input_fasta/"
-sample_list = "sample_list.txt"
-tmp_dir = "tmp_dir/"
-blast_db = "blast_db/composite"
-blast_homology = "96"
-core_no = "14"
-
-#log_file = open(tmp_dir + Run_Log.txt, 'w')
-
-#log_file.write("##Parameters used in this analysis run##")
-
-call(["rm", "-r", "tmp_dir"])
-call(["mkdir", tmp_dir])
-call(["mkdir", tmp_dir + "/abundance_files"])
-call(["mkdir", tmp_dir + "/cdhit_files"])
-call(["mkdir", tmp_dir + "/blast_files"])
-call(["mkdir", tmp_dir + "/html_files"])
-
-
-open_sample_list = open(sample_dir + sample_list, "rU")
-
-
-for sample in open_sample_list:
-    rows = sample.split('\t') 
-    sampleID =  rows[0]
-
-    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    print "Running prompt_sample.py on sample " + sampleID + " (" + rows[1].rstrip() + ")"
-    call(["scripts/prompt_sample.py", sample_dir, sampleID, tmp_dir, blast_db, blast_homology, core_no])
+print "Reading configuration file from " + conf_file
+Config.read(conf_file)
+  
+def main():
+   
+    # assign config parameters
+    sample_dir = ConfigSectionMap("locations")['sample_dir']
+    sample_list = ConfigSectionMap("locations")['sample_list']
+    tmp_dir = ConfigSectionMap("locations")['tmp_dir']
+    blast_db = ConfigSectionMap("locations")['blast_db']
+    blast_homology = ConfigSectionMap("parameters")['blast_homology'] 
+    core_no = ConfigSectionMap("parameters")['core_no']
+    
+    #log_file = open(tmp_dir + Run_Log.txt, 'w')
+    #log_file.write("##Parameters used in this analysis run##")
+    
+    call(["rm", "-r", "tmp_dir"])
+    call(["mkdir", tmp_dir])
+    call(["mkdir", tmp_dir + "/abundance_files"])
+    call(["mkdir", tmp_dir + "/cdhit_files"])
+    call(["mkdir", tmp_dir + "/blast_files"])
+    call(["mkdir", tmp_dir + "/html_files"])
+    
+    
+    open_sample_list = open(sample_list, "rU")
+    
+    
+    for sample in open_sample_list:
+        rows = sample.split('\t') 
+        sampleID =  rows[0]
+    
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        print "Running prompt_sample.py on sample " + sampleID + " (" + rows[1].rstrip() + ")"
+        call(["scripts/prompt_sample.py", sample_dir, sampleID, tmp_dir, blast_db, blast_homology, core_no])
+    
+    
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+    
+if __name__ == '__main__':
+    main()
